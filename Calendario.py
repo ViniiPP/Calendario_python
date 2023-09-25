1401b6dca199f40ccd786724413904fd034eaf34
ViniiPP <vinippolli@gmail.com>
2023-09-25 02:48:43 -0300

    calendar

---
+++ Calendario.py
@@ -0,0 +1,12 @@
+import calendar
+
+try: # É TIPO UM WHILE PORÉM MELHOR
+    entrada = int(input("Digite um ano para o calendário: "))
+    if (1 > entrada or entrada > 9999):  #CASO DO ERRO
+        print("Escolha outro ano")
+    else:                                #CASO DE CERTO
+        for i in range(12):
+            print(calendar.month(entrada, i+1))
+            print("--------------------")
+except:                                  #CASO NÃO DE ENTRADA EM ALGUM VALOR
+    print("Você não escolheu um ano")
